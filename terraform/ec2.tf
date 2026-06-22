ea4d8727c0736c142cb8e64737d1f0b7f98b31f6
Rohit Rawat <rohitrawat891997@gmail.com>
2026-06-23 02:53:47 +0530

    added terraform for this project!!

---
+++ terraform/ec2.tf
@@ -0,0 +1,82 @@
+
+# Key Value pair
+
+resource aws_key_pair my_key_pair {
+
+key_name="terra-automate-key-josh"
+public_key=file("terra-automate-key.pub")
+} 
+
+# VPC Default
+
+resource aws_default_vpc default {
+}
+
+# Security Group 
+
+resource aws_security_group my_security_group {
+
+name="terra-security-group"
+vpc_id= aws_default_vpc.default.id  # interpolation
+description = "this is Inbound and outbound rules for your instance Security group"
+
+}
+
+# Inbound & Outbount port rules
+
+
+
+resource aws_vpc_security_group_ingress_rule allow_http {
+  security_group_id = aws_security_group.my_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 8080
+  ip_protocol       = "tcp"
+  to_port           = 8080
+}
+
+resource aws_vpc_security_group_ingress_rule allow_ssh {
+  security_group_id = aws_security_group.my_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+
+resource aws_vpc_security_group_egress_rule allow_all_traffic {
+  security_group_id = aws_security_group.my_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+
+# EC2 instance
+
+
+resource aws_instance "my_instance" {
+
+	ami = "ami-0f8a61b66d1accaee" # OS AMI ID
+
+	instance_type = "t3.micro" # Instance Type
+
+	key_name = aws_key_pair.my_key_pair.key_name	# Key pair
+
+  depends_on = [
+    aws_key_pair.my_key_pair
+  ]
+
+	vpc_security_group_ids = [aws_security_group.my_security_group.id] # VPC & Security Group
+
+  user_data = file("script.sh")
+	
+	# root storage (EBS)
+	root_block_device {
+		volume_size = 30
+
+		volume_type = "gp3"
+	}
+
+	tags = {
+    Name = "terra-automate-server"
+  }
+}
---
+++ terraform/script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update
+sudo apt install docker.io -y
+sudo apt install docker-compose-v2 -y
+sudo usermod -aG  docker  ubuntu
+newgrp docker
+
