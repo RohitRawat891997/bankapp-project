a805e0e839d0d61e56bbbf1185e4760198a96e36
rohit <rohitrawat891997@gmail.com>
2026-06-23 16:35:54 +0530

    Change default AMI ID in variables.tf
    Updated the default AMI ID for the EC2 instance.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-085f9c64a9b75eed5"
+  default     = "ami-0f8a61b66d1accaee"
 }
 
 variable "instance_type" {
