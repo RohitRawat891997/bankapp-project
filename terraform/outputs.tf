605e55705783bf1cf23392483cbdc70d08370fce
Rohit Rawat <rohitrawat891997@gmail.com>
2026-09-06 03:20:28 +0530

    Update outputs.tf: Simplify and consolidate outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,30 @@
-output "ec2_arn" {
-  description = "ARN of the EC2 instance"
-  value       = aws_instance.my_instance.arn
+# ============================================
+# Terraform Outputs
+# ============================================
+# These outputs display important information after terraform apply
+# You can view all outputs with: terraform output
+# Or individual outputs with: terraform output <name>
+
+# ============================================
+# EC2 Instance Outputs
+# ============================================
+
+output "instance_public_ip" {
+  description = "Public IP address to access the BankApp server"
+  value       = aws_instance.main.public_ip
 }
 
-output "ec2_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.my_instance.public_ip
+output "instance_id" {
+  description = "EC2 Instance ID (use for AWS Console lookup)"
+  value       = aws_instance.main.id
 }
 
-output "ec2_public_dns" {
-  description = "Public DNS address of the EC2 instance"
-  value       = aws_instance.my_instance.public_dns
+output "bankapp_url" {
+  description = "URL to access BankApp application (http://<IP>:8080)"
+  value       = "http://${aws_instance.main.public_ip}:8080"
+}
+
+output "ssh_connection_string" {
+  description = "SSH command to connect to the instance"
+  value       = "ssh -i terra-automate-key ec2-user@${aws_instance.main.public_ip}"
 }
